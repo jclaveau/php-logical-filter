7ae82ea6b76231718065fc2d052ff6256d70e9a2
Jean Claveau <jc@mediabong.com>
2017-12-25 13:59:17 +0100

    Tests for complexe rules with nested operations

--- tests/CustomFilterTest.php
+++ tests/CustomFilterTest.php
@@ -3,6 +3,13 @@ namespace JClaveau\CustomFilter;
 
 use JClaveau\VisibilityViolator\VisibilityViolator;
 
+use JClaveau\CustomFilter\Rule\OrRule;
+use JClaveau\CustomFilter\Rule\AndRule;
+use JClaveau\CustomFilter\Rule\InRule;
+use JClaveau\CustomFilter\Rule\EqualRule;
+use JClaveau\CustomFilter\Rule\AboveRule;
+use JClaveau\CustomFilter\Rule\BelowRule;
+
 class CustomFilterTest extends \PHPUnit_Framework_TestCase
 {
     public static function setUpBeforeClass()
@@ -28,11 +35,11 @@ class CustomFilterTest extends \PHPUnit_Framework_TestCase
         );
 
         $this->assertEquals(
-            new Rule\AndRule([
-                new Rule\InRule('field', ['a', 'b', 'c']),
+            new AndRule([
+                new InRule('field', ['a', 'b', 'c']),
                 // new NotInRule(['a', 'b', 'c']),
-                new Rule\AboveRule('field', 3),
-                new Rule\BelowRule('field', 5)
+                new AboveRule('field', 3),
+                new BelowRule('field', 5)
             ]),
             $rules
         );
@@ -51,10 +58,47 @@ class CustomFilterTest extends \PHPUnit_Framework_TestCase
         ]);
 
         $this->assertEquals(
-            new Rule\AndRule([
-                new Rule\OrRule([
-                    new Rule\InRule('field', ['a', 'b', 'c']),
-                    new Rule\EqualRule('field', 'e')
+            new AndRule([
+                new OrRule([
+                    new InRule('field', ['a', 'b', 'c']),
+                    new EqualRule('field', 'e')
+                ]),
+            ]),
+            $filter->getRules()
+        );
+    }
+
+    /**
+     */
+    public function test_addRule_with_nested_operations()
+    {
+        $filter = new Filter();
+
+        $filter->addCompositeRule([
+            ['field', 'in', ['a', 'b', 'c']],
+            'or',
+            [
+                ['field', 'in', ['d', 'e']],
+                'and',
+                [
+                    ['field_2', 'above', 3],
+                    'or',
+                    ['field_3', 'below', -2],
+                ],
+            ],
+        ]);
+
+        $this->assertEquals(
+            new AndRule([
+                new OrRule([
+                    new InRule('field', ['a', 'b', 'c']),
+                    new AndRule([
+                        new InRule('field', ['d', 'e']),
+                        new OrRule([
+                            new AboveRule('field_2', 3),
+                            new BelowRule('field_3', -2),
+                        ]),
+                    ]),
                 ]),
             ]),
             $filter->getRules()
@@ -70,8 +114,8 @@ class CustomFilterTest extends \PHPUnit_Framework_TestCase
         $filter->addSimpleRule('field', 'in', ['a', 'b', 'c']);
 
         $this->assertEquals(
-            new Rule\AndRule([
-                new Rule\InRule('field', ['a', 'b', 'c'])
+            new AndRule([
+                new InRule('field', ['a', 'b', 'c'])
             ]),
             $filter->getRules()
         );
