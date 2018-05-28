01a79efae4d42167acc971a09b6bd1f332f72f7e
Jean Claveau <jc@mediabong.com>
2018-05-28 21:00:13 +0200

    adding spl_object_id polyfill

---
+++ src/functions.php
@@ -0,0 +1,13 @@
+<?php
+if (!function_exists('spl_object_id')) {
+    /**
+     * @see https://secure.php.net/manual/en/function.spl-object-id.php
+     * This method doesn't exist before PHP 7.2.0
+     */
+    function spl_object_id($object)
+    {
+        ob_start();
+        var_dump($object); // object(foo)#INSTANCE_ID (0) { }
+        return preg_replace('~.+#(\d+).+~s', '$1', ob_get_clean());
+    }
+}
