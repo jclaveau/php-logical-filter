ab5248b0295e8266a4f3ddd5a067be8155e8e421
Jean Claveau <jean.claveau@gmail.com>
2019-03-14 16:25:00 +0100

    Completing rule descriptions basic tests

--- tests/functionnal/LogicalFilterTest_rules_descriptions_trait.php
+++ tests/functionnal/LogicalFilterTest_rules_descriptions_trait.php
@@ -5,6 +5,106 @@ trait LogicalFilterTest_rules_descriptions
 {
     /**
      */
+    public function test_add_equal()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', '=', 2]
+        );
+
+        $this->assertEquals(
+            ['field_1', '=', 2],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_below()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', '<', 2]
+        );
+
+        $this->assertEquals(
+            ['field_1', '<', 2],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_above()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', '>', 2]
+        );
+
+        $this->assertEquals(
+            ['field_1', '>', 2],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_and()
+    {
+        $filter = new LogicalFilter(
+            ['and',
+                ['field', '>', 3],
+                ['field', '<', 5],
+            ]
+        );
+
+        $this->assertEquals(
+            ['and',
+                ['field', '>', 3],
+                ['field', '<', 5],
+            ],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_or()
+    {
+        $filter = new LogicalFilter(
+            ['or',
+                ['field', '>', 3],
+                ['field', '<', 5],
+            ]
+        );
+
+        $this->assertEquals(
+            ['or',
+                ['field', '>', 3],
+                ['field', '<', 5],
+            ],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_not()
+    {
+        $filter = new LogicalFilter(
+            ['not',
+                ['field', '>', 3],
+            ]
+        );
+
+        $this->assertEquals(
+            ['not',
+                ['field', '>', 3],
+            ],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
     public function test_add_in()
     {
         $filter = new LogicalFilter(
@@ -59,6 +159,8 @@ trait LogicalFilterTest_rules_descriptions
         );
     }
 
+    /**
+     */
     public function test_add_not_in()
     {
         $filter = new LogicalFilter(
@@ -67,6 +169,76 @@ trait LogicalFilterTest_rules_descriptions
 
         $this->assertEquals(
             ['field_1', '!in', [2, 3]],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_between()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', '><', [2, 3]]
+        );
+
+        $this->assertEquals(
+            ['field_1', '><', [2, 3]],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_between_or_equals_lower()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', '=><', [2, 3]]
+        );
+
+        $this->assertEquals(
+            ['field_1', '=><', [2, 3]],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_between_or_equals_upper()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', '><=', [2, 3]]
+        );
+
+        $this->assertEquals(
+            ['field_1', '><=', [2, 3]],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_between_or_equals_both()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', '=><=', [2, 3]]
+        );
+
+        $this->assertEquals(
+            ['field_1', '=><=', [2, 3]],
+            $filter->toArray()
+        );
+    }
+
+    /**
+     */
+    public function test_add_regexp()
+    {
+        $filter = new LogicalFilter(
+            ['field_1', 'regexp', '/^lalala*/']
+        );
+
+        $this->assertEquals(
+            ['field_1', 'regexp', '/^lalala*/'],
             $filter->toArray()
         );
     }
